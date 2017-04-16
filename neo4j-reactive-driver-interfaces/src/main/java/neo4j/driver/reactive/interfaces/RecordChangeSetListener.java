d2b900056e4b087a9957510aa7dfd0f2a5b541f0
vpeter93 <vpeter93@hotmail.com>
2017-04-16 10:54:42 +0200

    fix Add an Annotation to neo4j-drivers neo4j-reactive-driver-interfaces src/.../driver/reactive/interfaces/RecordChangeSetListener.java issue #6

--- neo4j-reactive-driver-interfaces/src/main/java/neo4j/driver/reactive/interfaces/RecordChangeSetListener.java
+++ neo4j-reactive-driver-interfaces/src/main/java/neo4j/driver/reactive/interfaces/RecordChangeSetListener.java
@@ -1,7 +1,7 @@
 package neo4j.driver.reactive.interfaces;
 
 import neo4j.driver.reactive.data.RecordChangeSet;
-
+@FunctionalInterface
 public interface RecordChangeSetListener {
 
 	void notify(RecordChangeSet rcs);
