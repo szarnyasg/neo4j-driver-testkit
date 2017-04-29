c2720e5b68137fefc45e72e1f12e2359a2f222fe
Kocsi Petra <Kocsi Petra>
2017-04-29 21:38:44 +0200

    commit to issue #64

--- neo4j-reactive-driver-impl/src/test/java/neo4j/driver/reactive/Neo4jReactiveSessionTest.java
+++ neo4j-reactive-driver-impl/src/test/java/neo4j/driver/reactive/Neo4jReactiveSessionTest.java
@@ -5,11 +5,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+
+import static org.neo4j.driver.v1.Values.parameters;
 
 import org.junit.Assert;
 import neo4j.driver.reactive.impl.Neo4jReactiveSession;
 import neo4j.driver.testkit.EmbeddedTestkitDriver;
+import neo4j.driver.util.PrettyPrinter;
 
 public class Neo4jReactiveSessionTest 
 {
@@ -30,7 +36,33 @@ public class Neo4jReactiveSessionTest
 		Assert.assertTrue(session.isOpen());//EmbeddedTestKitSession.isOpen always true and this is a wrapper
 	}
 	@Test(expected=UnsupportedOperationException.class)
-	public void lastBookmarkTest(){
+	public void lastBookmarkTest1(){
+		
+	
 		session.lastBookmark();//EmbeddedTestKitSession.lastBookmark always throw Exception and this is a wrapper
+		
+	}
+	
+	
+	
+	
+	@Test
+	public void run1Test(){
+		
+		Transaction transaction = session.beginTransaction();
+		session.run("CREATE (n:Label)");
+		transaction.success();
+		StatementResult statementResult = session.run("MATCH (n:Label) RETURN n");
+		while (statementResult.hasNext()) {
+			Record record = statementResult.next();
+			System.out.println(PrettyPrinter.toString(record));
+			System.out.println("GOOD run(String s) test");
+		}
+	}
+	
+	@Test
+	public void isEncryptedTest(){
+		//We always get false from this method
+		Assert.assertFalse(embeddedTestkitDriver.isEncrypted());
 	}
 }
