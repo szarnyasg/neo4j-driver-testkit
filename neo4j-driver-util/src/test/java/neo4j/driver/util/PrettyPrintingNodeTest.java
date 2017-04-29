a43e6e369d2e960ec5bfbbef55b81e327407a5cf
Kocsi Petra <Kocsi Petra>
2017-04-29 22:15:19 +0200

    commit to issues #25 #29 #30

--- neo4j-driver-util/src/test/java/neo4j/driver/util/PrettyPrintingNodeTest.java
+++ neo4j-driver-util/src/test/java/neo4j/driver/util/PrettyPrintingNodeTest.java
@@ -1,7 +1,10 @@
 package neo4j.driver.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.text.html.parser.Entity;
 
 import org.junit.Test;
 import org.neo4j.driver.internal.InternalNode;
@@ -20,14 +23,34 @@ public class PrettyPrintingNodeTest {
 
 	@Test
 	public void testName() throws Exception {
-		List<String> labels = ImmutableList.of("Person");
-		Map<String, Value> nodeProperties = ImmutableMap.of("name", Values.value("John Doe"));
+		List<String> labels = ImmutableList.of(null);
+		Map<String, Value> nodeProperties = ImmutableMap.of("", Values.value("John Doe"));
 		Node node = new InternalNode(1, labels, nodeProperties);
 		System.out.println(PrettyPrinter.toString(node));
 
-		Map<String, Value> relationshipProperties = ImmutableMap.of("weight", Values.value(2));
+		Map<String, Value> relationshipProperties = ImmutableMap.of("", Values.value(2));
 		Relationship rel = new InternalRelationship(5, 1, 2, "REL", relationshipProperties);
 		System.out.println(PrettyPrinter.toString(rel));
+	}
+	
+	@Test
+	public void toStringListTest(){
+		List<String> labels = ImmutableList.of("Person1");
+		List<String> labels2 = ImmutableList.of("Person2");
+		Map<String, Value> relationshipProperties = ImmutableMap.of("weight", Values.value(2));
+		Map<String, Value> nodeProperties = ImmutableMap.of("name", Values.value("John Doe"));
+		List<org.neo4j.driver.v1.types.Entity> testList=new ArrayList<>();
+		testList.add(new InternalNode(1,labels,nodeProperties));
+		testList.add(new InternalRelationship(5, 1, 2, "REL", relationshipProperties));
+		testList.add(new InternalNode(1,labels2,nodeProperties));
+		System.out.println("Test: toStringListTest "+PrettyPrinter.toString(testList));
+	}
+	
+	@Test
+	public void toStringRelationshipTest(){
+		Map<String, Value> relationshipProperties = ImmutableMap.of("weight", Values.value(2));
+		Relationship rel = new InternalRelationship(5, 1, 2, "REL", relationshipProperties);
+		System.out.println("Test: toStringRelationshipTest "+PrettyPrinter.toString(rel));
 	}
 
 }
